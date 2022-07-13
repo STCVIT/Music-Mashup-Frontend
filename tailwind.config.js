12dfc1df8b01282e283f67f17b3fe53046a13c87
Yashaswini Shivathaya <83407675+y-ashaswini@users.noreply.github.com>
2022-07-13 16:14:11 +0530

    Adding base files
    1) npm install - node modules
    2) react modules
    3) tailwind modules

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'blackone' : '#2A2A2A',
+        'whiteone' : '#F2F2F2',
+      },
+    },
+  },
+  plugins: [],
+}
