ad6fdcfd3e6ca9e6c5a0ea5bba1ed762a6440806
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-08-20 13:12:51 +0530

    drag drop minor changes

--- src/Components/DragDropOne.js
+++ src/Components/DragDropOne.js
@@ -22,23 +22,38 @@ export default function DragDropOne() {
     const dragStyle=<div className="flex flex-wrap items-center flex-col justify-between h-full w-full"><img alt = "Upload Cloud" src={cloud} className=" max-w-[4rem]"/>Drag and drop your files here</div>
 
 
+    const [search, setSearch] = useState('');
+
     return (
         <div className="bg-blackone min-w-screen min-h-screen relative overflow-hidden">
             <Link to='../LandingTwo'>
                 <div className="absolute right-[1rem] top-[1rem] max-w-[2rem]"><img src={home_white}/></div>
             </Link>
             <img alt = "BG Design" src={dragdrop} className="absolute"/>
-            <div className="w-[50%] bg-whiteone py-[1rem] mx-auto rounded-lg"><img src={search_black}/>Browse on Web</div>
+            
             <div className = "absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-2 rounded-lg min-h-[75%] min-w-[50%] text-whiteone">
+
+            <form className="bg-whiteone text-blackone p-[0.8rem] my-[2%] mx-[5%] rounded-lg flex flex-wrap">
+            <img src={search_black} className="max-w-[1.5rem] max-h-[1.5rem] mr-[1rem]"/>
+            <input
+                        type="text"
+                        value={search}
+                        placeholder="Browse on web"
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="bg-whiteone px-[0.5rem] text-blackone w-[75%]">
+                        
+            </input>
+            </form>
+
                 <div className = "relative m-[5%] py-[5rem] border-2 rounded-lg border-dashed cursor-pointer">
-                    <FileUploader 
+                    <FileUploader
                         handleChange={handleChange}
                         name="file"
                         types={fileTypes}
                         multiple={true}
                         hoverTitle="Drop Here"
                         minSize={2}
-                        children={dragStyle}
+                        // children={dragStyle}
                     />
                     
                 </div>
