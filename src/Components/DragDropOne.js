34d9144cf3d4a14abc7c9c5de5a1c776d1ad340d
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-09-18 17:44:46 +0530

    firestore : delete all files after refresh

--- src/Components/DragDropOne.js
+++ src/Components/DragDropOne.js
@@ -5,25 +5,25 @@ import { Link } from "react-router-dom";
 import dragdrop from "../Images/dragdrop-bg.png";
 import cloud from "../Images/cloud.png";
 import dustbin_inactive from "../Images/dustbin_inactive.png";
+import dustbin_active from "../Images/dustbin_active.png";
 import mash_btn_inactive from "../Images/mash_btn_inactive.png";
+import mash_btn_active from "../Images/mash_btn_active.png";
 import home_white from "../Images/home_white.png";
 import search_black from "../Images/search_black.png";
 import cdadd from "../Images/cdadd.png";
 import { useUserAuth } from "../context/UserAuthContext";
 
-<<<<<<< HEAD
 import { storage } from "../firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import { v4 } from "uuid";
-import BlackScreenAnimation from "./BlackScreenAnimation";
-=======
-
-import { storage } from '../firebase';
-import { ref, uploadBytes, listAll, getDownloadURL} from 'firebase/storage';
-import { v4 } from 'uuid';
 import Navbar from "./Navbar";
-
->>>>>>> 5d322a40298bc71fc0512caf404d6cd6dc1799e4
+import BlackScreenAnimation from "./BlackScreenAnimation";
 
 export default function DragDropOne() {
   // const fileTypes = ["WAV"];
@@ -40,55 +40,55 @@ export default function DragDropOne() {
   const [audioUpload, setAudioUpload] = useState(null);
   const audioListRef = ref(storage, "audio/");
 
-<<<<<<< HEAD
   const uploadAudio = () => {
     if (audioUpload == null) return;
-    if (audioList.length > 3) {
+    if (audioList.length > 2) {
       alert("maximum 3 songs only!");
       return;
-=======
-    // const dragStyle=<div className="flex flex-wrap items-center flex-col justify-between h-full w-full"><img alt = "Upload Cloud" src={cloud} className=" max-w-[4rem]"/>Drag and drop your files here</div>
-
-    
-    const [search, setSearch] = useState('');
-    const [audioList, setAudioList] = useState([]);
-    const [audioUpload, setAudioUpload] = useState(null);
-    const audioListRef = ref(storage, 'audio/');
-    const uploadAudio = () => {
-        if(audioUpload == null) return;
-        const audioRef = ref(storage, `audio/${audioUpload.name +"___"+ v4()}`);
-        uploadBytes(audioRef, audioUpload).then((snapshot)=>{
-            getDownloadURL(snapshot).then((url)=>{
-                setAudioList((prev)=>[...prev, url]);
-                alert("audio uploaded!");
-            })
-            
-
-        });
->>>>>>> 5d322a40298bc71fc0512caf404d6cd6dc1799e4
     }
+    console.log("uploading file...");
 
     const audioRef = ref(storage, `audio/${audioUpload.name + "___" + v4()}`);
     uploadBytes(audioRef, audioUpload).then((snapshot) => {
-      getDownloadURL(snapshot).then((url) => {
-        setAudioList((prev) => [...prev, url]);
-        alert("audio uploaded!");
-      });
+      console.log("uploaded: ", snapshot.metadata);
+      // const fullname = item.fullPath;
+      // const temp = fullname.split("__")[0];
+      // const audioname = temp.slice(6, temp.length);
+      getDownloadURL(snapshot.ref)
+        .then((urll) => {
+          setAudioList((prev) => [
+            ...prev,
+            // {
+            //   filename: fullname,
+            //   name: audioname,
+            //   url: urll,
+            // },
+            urll,
+          ]);
+          alert("audio uploaded!");
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
     });
   };
 
-<<<<<<< HEAD
   useEffect(() => {
+    // console.log("use effect run");
     listAll(audioListRef).then((res) => {
+      // console.log(res.items);
+
       res.items.forEach((item) => {
-        const temp = item.fullPath.split("__")[0];
+        const fullname = item.fullPath;
+        const temp = fullname.split("__")[0];
         const audioname = temp.slice(6, temp.length);
         getDownloadURL(item).then((audiourl) => {
           // async await use state - read. promiseall use.
           //   console.log("url:", url);
           setAudioList((prev) => [
-            ...prev, // set functions do not work asynchronously
+            ...prev, // set functions in React do not work asynchronously
             {
+              filename: fullname,
               name: audioname,
               url: audiourl,
             },
@@ -99,18 +99,51 @@ export default function DragDropOne() {
   }, []);
 
   //   const b = document.querySelector(".mash-btn").classList;
-=======
-    return (
-        
-        <div className="bg-blackone min-w-screen min-h-screen relative overflow-hidden">
-            
-            <Navbar />
->>>>>>> 5d322a40298bc71fc0512caf404d6cd6dc1799e4
+
+  // addEventListener('beforeunload', (event) => {
+
+  // });
+  function handleDelete() {
+    console.log("delete button pressed");
+    // audioList.map((eachAudio)=>{
+    //   var storageRef = ref(storage, eachAudio.filename);
+    //   deleteObject(storageRef)
+    //   .then(() => {
+    //     console.log("deleted all files");
+    //   })
+    //   .catch((error) => {
+    //     console.log("error: ", error);
+    //   });
+    // })
+  }
+
+  onbeforeunload = (event) => {
+    event.preventDefault();
+    // console.log("before refresh: ", event);
+    audioList.map((eachAudio) => {
+      var storageRef = ref(storage, eachAudio.filename);
+      deleteObject(storageRef)
+        .then(() => {
+          console.log("deleted all files");
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
+    });
+  };
+
+  // storageRef.listAll().then((res) => {
+  //   res.forEach((item) => {
+  //     console.log(item);
+  //   });
+  // }).catch((e)=>{
+  //   console.log("errors: ",e);
+  // });
 
   return (
     <div>
       <BlackScreenAnimation />
-
+      {/* <Navbar /> */}
       <div className="bg-blackone min-w-screen min-h-screen relative overflow-hidden">
         <Link to="../LandingTwo">
           <motion.div
@@ -123,7 +156,7 @@ export default function DragDropOne() {
         </Link>
 
         {/* <Link to='../LandingTwo'> */}
-        <motion.div
+        {/* <motion.div
           whileTap={{ scale: 0.9 }}
           className="absolute flex align-middle gap-2 right-0 top-[1rem] z-[20] "
         >
@@ -135,16 +168,16 @@ export default function DragDropOne() {
             src={`${user.photoURL}`}
             alt="user img"
           />
-        </motion.div>
+        </motion.div> */}
         {/* </Link> */}
         {/* user ? userImg : user.photoURL */}
 
         <img alt="BG Design" src={dragdrop} className="absolute" />
 
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-2 rounded-lg min-h-[75%] min-w-[50%] text-whiteone">
+        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-2 rounded-lg min-h-[60%] w-[50%] text-whiteone p-5">
           {/* Search on Youtube */}
 
-          <form className="bg-whiteone text-blackone px-[0.5rem] py-[0.2rem] my-[2%] mx-[5%] rounded-lg flex flex-wrap">
+          <form className="bg-whiteone text-blackone px-[0.5rem] py-[0.5rem] my-[2%] rounded-lg flex flex-wrap">
             <img
               src={search_black}
               className="max-w-[1.5rem] max-h-[1.2rem] my-auto mr-[1rem]"
@@ -161,7 +194,8 @@ export default function DragDropOne() {
           {/* The Uploading Arena */}
 
           {/* <div className="relative m-[5%] py-[5rem] border-2 rounded-lg border-dashed cursor-pointer"> */}
-          <div className="flex justify-center">
+          <div className="flex flex-col align-middle text-center justify-center">
+            {/* <div className="flex"> */}
             <input
               type="file"
               accept=".wav, .mp3"
@@ -169,6 +203,14 @@ export default function DragDropOne() {
                 setAudioUpload(event.target.files[0]);
               }}
             />
+            {/* </div> */}
+
+            <button
+              onClick={uploadAudio}              
+              className="my-5 mx-[40%] border-whiteone border-2 hover:bg-whiteone hover:text-blackone rounded-lg px-2 py-1 text-white font-bold"
+            >
+              Upload Audio
+            </button>
           </div>
 
           {/* <FileUploader
@@ -181,7 +223,7 @@ export default function DragDropOne() {
                         // children={dragStyle}
                     /> */}
           {/* </div> */}
-          <div className="displayaudio grid grid-cols-4">
+          <div className="displayaudio grid grid-cols-5">
             {audioList &&
               audioList.map((a) => (
                 <div className="flex flex-col align-middle text-center">
@@ -199,25 +241,21 @@ export default function DragDropOne() {
           </div>
 
           <img
+            onClick={handleDelete}
             alt="Inactive Delete Button"
-            src={dustbin_inactive}
-            className="absolute right-1 bottom-1 max-w-[2rem]"
+            src={audioList ? dustbin_active : dustbin_inactive}
+            className="cursor-pointer absolute right-1 bottom-1 max-w-[2rem]"
           />
-
-          <button
-            onClick={uploadAudio}
-            className="absolute border-whiteone border-2 hover:bg-whiteone hover:text-blackone bottom-1 left-1 rounded-lg px-2 py-1 text-white font-bold"
-          >
-            Upload Audio
-          </button>
         </div>
 
         <Link to="../MashingOne">
-          <div className="mash-btn absolute bottom-10 left-[50%] translate-x-[-50%] text-blackone bg-whiteone border-2 px-3 py-1 rounded-lg min-w-[7rem] mx-auto my-auto text-center font-extrabold">
-            {/* {console.log(b)} */}
-            {/* {audioList.length === 0 && b.classList.add("opacity-50")} */}
-            {audioList.length === 0 ? `CAN'T MASH` : `MASH`}
-          </div>
+          {/* {console.log(b)} */}
+          {/* {audioList.length === 0 && b.classList.add("opacity-50")} */}
+          <img
+            className="max-w-[7rem] absolute bottom-10 left-[50%] translate-x-[-50%]"
+            src={audioList.length === 0 ? mash_btn_inactive : mash_btn_active}
+          />
+          {/* {audioList.length === 0 ? `CAN'T MASH` : `MASH`} */}
         </Link>
       </div>
     </div>
