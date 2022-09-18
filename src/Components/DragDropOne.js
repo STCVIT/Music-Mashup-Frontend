716b5935898478a868b87069c56c1c38531d7b9b
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-09-18 18:40:35 +0530

    adding to prev 2

--- src/Components/DragDropOne.js
+++ src/Components/DragDropOne.js
@@ -11,6 +11,7 @@ import mash_btn_active from "../Images/mash_btn_active.png";
 import home_white from "../Images/home_white.png";
 import search_black from "../Images/search_black.png";
 import cdadd from "../Images/cdadd.png";
+import youtube from "../Images/youtube.png";
 import { useUserAuth } from "../context/UserAuthContext";
 
 import { storage } from "../firebase";
@@ -76,8 +77,6 @@ export default function DragDropOne() {
   useEffect(() => {
     // console.log("use effect run");
     listAll(audioListRef).then((res) => {
-      
-
       res.items.forEach((item) => {
         const fullname = item.fullPath;
         const temp = fullname.split("__")[0];
@@ -146,6 +145,10 @@ export default function DragDropOne() {
       {/* <Navbar /> */}
       <div className="bg-blackone min-w-screen min-h-screen relative overflow-hidden">
         <Link to="../LandingTwo">
+          {/* <div id="modal" className="absolute left-[50%] translate-x-[-50%] top-[2rem] z-50 bg-whiteone rounded-lg flex justify-center text-center px-5 py-2">
+          <div className="font-bold my-auto">LOADING ...</div>
+        </div> */}
+
           <motion.div
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.9 }}
@@ -177,24 +180,27 @@ export default function DragDropOne() {
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-2 rounded-lg min-h-[60%] w-[50%] text-whiteone p-5">
           {/* Search on Youtube */}
 
-          <form className="bg-whiteone text-blackone px-[0.5rem] py-[0.5rem] my-[2%] rounded-lg flex flex-wrap">
+          <form className="justify-between bg-whiteone text-blackone px-[0.5rem] py-[0.5rem] mb-[1rem] rounded-lg flex flex-wrap">
             <img
-              src={search_black}
-              className="max-w-[1.5rem] max-h-[1.2rem] my-auto mr-[1rem]"
+              src={youtube}
+              className="max-w-[1.5rem] max-h-[1.2rem] my-auto mx-2"
             />
             <input
               type="text"
               value={search}
-              placeholder="Browse on web"
+              placeholder="Get music from Youtube"
               onChange={(e) => setSearch(e.target.value)}
               className="outline-none bg-whiteone px-[0.5rem] text-blackone w-[75%]"
             ></input>
+            <img
+              src={search_black}
+              className="max-w-[1.5rem] max-h-[1.2rem] my-auto mx-2"
+            />
           </form>
 
-          {/* The Uploading Arena */}          
+          {/* The Uploading Arena */}
 
-          <div className="flex flex-col align-middle text-center justify-center">
-            
+          <div className="flex flex-col font-bold align-middle text-center justify-center">
             <input
               type="file"
               accept=".wav, .mp3"
@@ -203,10 +209,9 @@ export default function DragDropOne() {
               }}
             />
 
-
             <button
-              onClick={uploadAudio}              
-              className="my-5 mx-[40%] border-whiteone border-2 hover:bg-whiteone hover:text-blackone rounded-lg px-2 py-1 text-white font-bold"
+              onClick={uploadAudio}
+              className="my-5 mx-[30%] border-whiteone border-2 hover:bg-whiteone hover:text-blackone rounded-lg px-2 py-1 text-white font-bold"
             >
               Upload Audio
             </button>
@@ -221,7 +226,6 @@ export default function DragDropOne() {
                         minSize={2}
                         // children={dragStyle}
                     /> */}
-          
 
           <div className="displayaudio grid grid-cols-5">
             {audioList &&
@@ -235,26 +239,24 @@ export default function DragDropOne() {
                   type="audio/wav"
                 ></audio> */}
                   <img src={cdadd} />
-                  <div className="text-[0.8rem]">{a.name}</div>
+                  <div className="text-whiteone text-[0.8rem]">{a.name}</div>
                 </div>
               ))}
           </div>
-
           <img
             onClick={handleDelete}
             alt="Inactive Delete Button"
-            src={audioList ? dustbin_active : dustbin_inactive}
+            src={audioList ? dustbin_inactive : dustbin_active}
+            // src={dustbin_inactive}
             className="cursor-pointer absolute right-1 bottom-1 max-w-[2rem]"
           />
         </div>
 
         <Link to="../MashingOne">
-
           <img
             className="max-w-[7rem] absolute bottom-10 left-[50%] translate-x-[-50%]"
             src={audioList.length === 0 ? mash_btn_inactive : mash_btn_active}
           />
-          
         </Link>
       </div>
     </div>
