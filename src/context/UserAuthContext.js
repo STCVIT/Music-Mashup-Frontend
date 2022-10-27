6f6ed938d4b08e9201e1b30ffeb6353fcf2c1f99
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-10-27 23:16:22 +0400

    small changes

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -26,24 +26,108 @@ export function UserAuthContextProvider({ children }) {
   function signIn(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
   }
-  
+
   function logOut() {
     return signOut(auth);
   }
 
   function googleSignIn() {
-    var credential = auth.GoogleAuthProvider.credential();
-    
-      auth()
-      .currentUser.linkWithCredential(credential)
-      .then(
-        function (user) {
-          console.log("Anonymous account successfully upgraded", user);
-        },
-        function (error) {
-          console.log("Error upgrading anonymous account", error);
-        }
-      );
+    console.log("pressed");
+    const googleAuthProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleAuthProvider)
+      .then((result) => {
+        const credentials = GoogleAuthProvider.credentialFromResult(result);
+        console.log("credential generated");
+        const id_token = credentials.accessToken;
+        console.log("id token: ", id_token);
+        const googleUser = result.user;
+        //i think not sure, the error message
+        const credential = GoogleAuthProvider.credential(
+          googleUser.getAuthResponse(true).id_token
+        );
+        // const credential = GoogleAuthProvider.credential(googleUser.get().getAuthResponse().id_token);
+        // const credential = GoogleAuthProvider.credential(id_token);
+        linkWithCredential(auth.currentUser, credential)
+          .then((usercred) => {
+            const userf = usercred.user;
+            console.log("Anonymous account successfully upgraded", userf);
+          })
+          .catch((error) => {
+            console.log("Error upgrading anonymous account", error);
+          });
+      })
+
+      //mene thoda sa code change kiya line 48,
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // const emailerr = error.customData.email;
+        const credentialerr = GoogleAuthProvider.credentialFromError(error);
+        console.log("errors: ");
+        console.log(errorCode);
+        console.log(errorMessage);
+        // console.log(emailerr);
+        console.log(credentialerr);
+      });
+
+    // aap kar
+    // return signInWithPopup(auth, googleAuthProvider);
+    // const temp2 = signInWithPopup(auth, googleAuthProvider);
+    // console.log("temp2:", temp2);
+    // console.log(auth.currentUser);
+    //   signInWithPopup(auth, googleAuthProvider).then(
+    //     (result) => {
+    //   // This gives you a Google Access Token. You can use it to access the Google API.
+    //   const credential = GoogleAuthProvider.credential(result)
+    //   const token = credential.accessToken;
+    //   // The signed-in user info.
+    //   // const user = result.user;
+    //   // ...
+    //   }).catch((error) => {
+    //   // Handle Errors here.
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   const email = error.customData.email;
+    //   // The AuthCredential type that was used.
+    //   const credential = GoogleAuthProvider.credentialFromError(error);
+    //   // ...
+    // }
+    //   );
+
+    // console.log("in google sign in ");
+    // const credential = GoogleAuthProvider.credential(
+    //   user.getAuthResponse().token
+    // );
+    //   console.log("in line 39");
+    // linkWithCredential(auth.currentUser, credential)
+    //   .then((usercred) => {
+    //     const userf = usercred.user;
+    //     console.log("Anonymous account successfully upgraded", userf);
+    //   })
+    //   .catch((error) => {
+    //     console.log("Error upgrading anonymous account", error);
+    //   });
+
+    // auth.currentUser.linkWithCredential(credential).then(
+    //   function (user) {
+    //     console.log("Anonymous account successfully upgraded", user);
+    //   },
+    //   function (error) {
+    //     console.log("Error upgrading anonymous account", error);
+    //   }
+    // );
+
+    // var credential = auth.GoogleAuthProvider.credential();
+
+    // auth.currentUser.linkWithCredential(credential).then(
+    //   function (user) {
+    //     console.log("Anonymous account successfully upgraded", user);
+    //   },
+    //   function (error) {
+    //     console.log("Error upgrading anonymous account", error);
+    //   }
+    // );
 
     // const provider = new auth.GoogleAuthProvider();
     // console.log("provider: ",provider);
@@ -61,8 +145,6 @@ export function UserAuthContextProvider({ children }) {
     //     console.log("Error upgrading anonymous account", error);
     //   }
     // );
-    // const googleAuthProvider = new GoogleAuthProvider();
-    // return signInWithPopup(auth, googleAuthProvider);
   }
 
   function setTokenFunc(x) {
